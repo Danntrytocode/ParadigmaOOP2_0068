0ec741f5a215eefe53549e978b95a520be90f343
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 18:19:52 +0700

    idk what is this

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 class Orang {
     public:
-        int umur;
-        Orang (int pUmur);
+        int Umur;
+        Orang (int pUmur) :
+            Umur(pUmur){
+            cout << "Orang dibuat dengan umur " << Umur << "\n" << endl;
+        }
 };
+
 class Pekerja : virtual public Orang{};
 class Pelajar : virtual public Orang{};
 class Ucup : public Pekerja, public Pelajar{};
