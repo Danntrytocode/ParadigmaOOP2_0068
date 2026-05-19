01bf2eea829db2df15d67929f0292ab77492dcc3
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 18:16:44 +0700

    add member variabel dan constructor

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 
-class Orang {};
+class Orang {
+    public:
+        int umur;
+        Orang (int pUmur);
+};
 class Pekerja : virtual public Orang{};
 class Pelajar : virtual public Orang{};
 class Ucup : public Pekerja, public Pelajar{};
