3f0060235db030db0aac1f5d13f1730ef7fbb65e
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 17:04:07 +0700

    add function virtual dan create class Ucup,Lia

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+class Seseorang{
+    public:
+        virtual void pesan() = 0;
+        /*virtual void pesan(){
+            cout << "Pesan Dari seseorang" << endl;
+        }*/
+};
+
+class Ucup :public Seseorang{};
+class Lia :public Seseorang{}:
