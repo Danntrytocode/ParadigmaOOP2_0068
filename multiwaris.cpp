a879e6c5b1b31c620d0ace1e0ba61c96c29f6135
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 18:15:20 +0700

    membuat 4 class kosong

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 using namespace std;
+
+
+class Orang {};
+class Pekerja : virtual public Orang{};
+class Pelajar : virtual public Orang{};
+class Ucup : public Pekerja, public Pelajar{};
