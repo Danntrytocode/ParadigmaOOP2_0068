6446359e0b8b3d234280c19f76038d21189a7984
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 18:58:53 +0700

    multi waris but without virtual

--- Solvemultiwaris.cpp
+++ Solvemultiwaris.cpp
@@ -1,3 +1,43 @@
 #include <iostream>
 using namespace std;
 
+class Orang {
+    public:
+        int Umur;
+        Orang (int pUmur) :
+            Umur(pUmur)
+        {
+            cout << "Orang dibuat dengan umur " << Umur << "\n" << endl;
+        }
+};
+
+class Pekerja : public Orang {
+    public:
+        Pekerja(int pUmur) : Orang(pUmur){
+            cout << "Pekerja dibuat\n" << endl;
+        } 
+};
+
+class Pelajar : public Orang{
+    public:
+    Pelajar(int pUmur) : Orang(pUmur)
+    {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+};
+
+class Ucup : public Pekerja, public Pelajar{
+    public:
+    Ucup(int pUmur) :
+        Pekerja(pUmur),
+        Pelajar(pUmur)
+        //Orang(pUmur)   tidak menggunakan virtual maka tak perlu
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main(){
+    Ucup a(12);
+    return 0;
+}
