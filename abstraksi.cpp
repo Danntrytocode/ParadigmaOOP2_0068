aadf44906377667a525007eda884ff360ffa90f2
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-05-19 18:07:20 +0700

    add function untuk inserting nilai priv member

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,5 +5,11 @@ class AbstraksiKlass{
     private:
         string x;
         string y;
-    
+    public:
+        //method untuk mengisi nilai private member
+        void setXY(string a, string b){
+            x = a;
+            y = b;     
+        }
+        
 };
